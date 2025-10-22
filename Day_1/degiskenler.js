6ebf4d9cbf52a48382463c05f2d5d49ab9e2ce33
Doğukan Burak Çelikten <dcelikten01@icloud.com>
2025-10-22 11:53:19 +0300

    day 2

---
+++ Day_1/degiskenler.js
@@ -0,0 +1,23 @@
+let ad="Burak";
+let soyad="Çelikten";
+let ulke="Türkiye";
+let sehir="İstanbul";
+let yas=22;
+let instagram="@dcelikten01";
+let evliMi=false;
+console.log(ad, soyad, ulke, sehir, yas, instagram, evliMi);
+
+const yerCekimi=9.81; // m/s² cinsinden yerçekimi ivmesi
+const pi=3.14;
+const kaynamaNoktasi=100; // Suyun kaynama noktası (°C)
+console.log(yerCekimi, pi, kaynamaNoktasi);
+
+// Yorumlar kodu daha okunabilir hale getirir
+// 30 günlük JavaScript challenge'ına hoş geldiniz!
+/* Yorumlar kodu daha okunur hale getirir ve şu an
+birden fazla satırda kod yazıyoruz */
+
+
+let infoBurak=`Benim adım ${ad} ${soyad}. ${yas} yaşındayım. ${sehir}, ${ulke}'de yaşıyorum. Instagram hesabım ${instagram}. Evli miyim? ${evliMi}.`;
+console.log(infoBurak);
+console.log(ad.length);
